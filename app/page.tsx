8f03b58f5f35ad26cecd2c1471ed15bf6b6f55fb
Arzeno <arzenoeugenio@gmail.com>
2024-11-11 17:32:03 -0300

    CRUD

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@/components/ui/button';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
 
@@ -11,26 +12,24 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 dark:bg-gray-900">
       <main className="flex flex-col items-center gap-8 text-center">
-        <h1 className="head_gradient">SetIA</h1>
+        <h1 className="head_text text-left mb-2"> <span className='head_gradient'> SetIA </span> </h1>
         <p className="text-xl text-gray-600 dark:text-gray-400">Manejo Inteligente de Datos</p>
 
         {!user ? (
-          <a
-            className="rounded-full button_gradient transition-colors"
-            href="/api/auth/login"
-          >
-            Login
-          </a>
+          <Button className="rounded-full button_gradient transition-colors">
+            <a href="/api/auth/login">
+              Login
+            </a>
+          </Button>
         ) : (
           <div className="flex flex-col items-center gap-4">
             <img src={user.picture ?? ""} className='w-24 h-24'  alt=""/>
             <p className="text-xl font-semibold text-gray-800 dark:text-gray-200">Bienvenido, {user.name}!</p>
-            <a
-              className="rounded-full button_gradient transition-colors"
-              href="/api/auth/logout"
-            >
+            <Button className="rounded-full button_gradient transition-colors">
+              <a href="/api/auth/logout">
               Logout
-            </a>
+              </a>
+            </Button>
           </div>
         )}
       </main>
